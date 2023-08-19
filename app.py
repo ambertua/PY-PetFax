55ff9488201c55534c2472b914fc825fd5df837f
ambertua <amber.tuamoheloa@yahoo.com>
2023-08-19 10:51:23 -0700

    first

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-# config                    
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# index route
+# Sample pet data from pets.json
+import json
+with open('pets.json') as json_file:
+    pets_data = json.load(json_file)
+
+# Index route
 @app.route('/')
-def index(): 
+def index():
     return 'Hello, this is PetFax!'
 
-# pets index route
+# Pets index route
 @app.route('/pets')
-def pets(): 
+def pets():
     return 'These are our pets available for adoption!'
+
+# Show page route for individual pets
+@app.route('/pets/<int:pet_id>')
+def show_pet(pet_id):
+    pet = next((pet for pet in pets_data if pet['pet_id'] == pet_id), None)
+    if pet:
+        return render_template('show_pet.html', pet=pet)
+    else:
+        return 'Pet not found'
+
+# Facts create page route
+@app.route('/facts/new')
+def create_fact():
+    return render_template('create_fact.html')
+
+if __name__ == '__main__':
+    app.run()
